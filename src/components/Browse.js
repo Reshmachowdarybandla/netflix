d3e3e9caa4fe36f4e2f302208a08429b8745330f
Reshmachowdarybandla <reshmachowdarybandla@gmail.com>
2024-07-27 23:12:04 +0530

    movie card

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,9 +3,13 @@ import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
+import useTopRated from "../hooks/useTopRated";
 const Browse = () =>{
     useNowPlayingMovies();
     usePopularMovies();
+    useUpcomingMovies();
+    useTopRated();
     return (
         <div>
             <Header/>
---
+++ src/hooks/useTopRated.js
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../utils/constants";
+import { useEffect } from "react";
+import { addTopRated } from "../utils/moviesSlice";
+
+const useTopRated = () =>{
+    const dispatch = useDispatch();
+    const getTopRated = async() => {
+        const data = await fetch('https://api.themoviedb.org/3/movie/top_rated?page=1', API_OPTIONS);
+        const json = await data.json();
+        dispatch(addTopRated(json.results)) ;
+    };
+    useEffect(() => {
+        getTopRated();
+    }, [])
+};
+
+export default useTopRated;
